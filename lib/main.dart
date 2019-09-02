e79651d1e9be5de25db4f35244abbc1355fdb7b2
bruno pf silva <bruno.pfsilva@hotmail.com>
2019-09-02 17:33:36 +0100

    construindo a tela de login

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:carros_app/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark
+      ),
+      home: loginPage(),
+    );
+  }
+}
+
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class loginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
